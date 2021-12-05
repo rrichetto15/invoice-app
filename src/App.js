e6ef40f7b9abb59baa0634a91a1941d65d16c235
Ryan Richetto <rrichetto15@gmail.com>
2021-12-05 02:39:26 -0800

    Setup Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Dashboard from './pages/dashboard/Dashboard';
+import Invoice from './pages/invoice/Invoice';
 
 const App = () => {
-  return <div>The App</div>;
+  return (
+    <div>
+      <h1>Sidebar</h1>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/invoice/:id" element={<Invoice />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
