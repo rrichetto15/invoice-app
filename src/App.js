524e4cc72e9a058b3a455355dc73f0563f79269f
Ryan Richetto <rrichetto15@gmail.com>
2021-12-05 03:25:13 -0800

    Create sidebar and add routes for login and signup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Dashboard from './pages/dashboard/Dashboard';
 import Invoice from './pages/invoice/Invoice';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/invoice/:id" element={<Invoice />} />
-      </Routes>
-    </div>
+    <AppLayout>
+      <Sidebar />
+      <Main>
+        <h1>Invoice App</h1>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/invoice/:id" element={<Invoice />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Main>
+    </AppLayout>
   );
 };
+
+const AppLayout = styled.div`
+  display: flex;
+`;
+
+const Main = styled.main`
+  max-width: 93rem;
+  width: 100%;
+  margin: 0 auto;
+  padding: 6rem 10rem 3rem 10rem;
+`;
 
 export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import logo from '../assets/logo.png';
+import sun from '../assets/icon-sun.svg';
+import avatar from '../assets/image-avatar.jpg';
+
+const Sidebar = () => {
+  return (
+    <SidebarWrap>
+      <Link to="/">
+        <img src={logo} alt="Invoice App" />
+      </Link>
+      <Navigation>
+        <div>
+          <button>
+            <img src={sun} alt="Sun" />
+          </button>
+        </div>
+        <div>
+          <button>
+            <img className="avatar" src={avatar} alt="Avatar" />
+          </button>
+        </div>
+      </Navigation>
+    </SidebarWrap>
+  );
+};
+
+const SidebarWrap = styled.div`
+  flex-shrink: 0;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 10rem;
+  height: 100vh;
+  background: var(--color-dark-3);
+  border-radius: 0 2rem 2rem 0;
+`;
+
+const Navigation = styled.nav`
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 9rem;
+  }
+
+  div:first-child {
+    border-bottom: 1px solid #494e6e;
+  }
+
+  button {
+  }
+
+  .avatar {
+    height: 4rem;
+    width: 4rem;
+    border-radius: 50%;
+  }
+`;
+
+export default Sidebar;
